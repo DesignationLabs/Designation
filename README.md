bdcc3dada0f5ba79c5c7131ef42baea4bcf2628e
Kevin Yun <yun.kevin.s@gmail.com>
2014-01-28 19:43:34 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Source code for Designation.io
---
+++ js/konami_code.js
@@ -0,0 +1,12 @@
+
+    var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+    $(document).keydown(function(e) {
+      kkeys.push( e.keyCode );
+      if ( kkeys.toString().indexOf( konami ) >= 0 ) {
+        $(document).unbind('keydown',arguments.callee);
+        // do something awesome
+        $('#cities').attr('src', 'img/icons_flat/cities2.png');
+        $('.konami_directions').css('opacity', '0.2');
+      }
+
+    });
